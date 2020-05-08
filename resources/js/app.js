90690b772d72228f9debff836141a093b0ce9b4c
Eric Bernardo <eric.bernardo.sousa@gmail.com>
2020-05-08 15:29:36 -0300

    Desenvolvido o menu, banner, mapa e rodapé

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 require('./bootstrap');
+
+const simpleParallax = require('simple-parallax-js');
+
+const image = document.getElementsByClassName('banner__img');
+
+new simpleParallax(image, {
+    scale: 1.5
+});
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,98 +3,47 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" href="{{URL::asset('/images/favicon.png')}}" />
 
-        <title>Laravel</title>
+        <title>Artefatos de cimento Portão</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="{{ mix('/css/app.css') . '?v=' . time() }}">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+        <header class="header">
+            <a href="/" class="logo">
+                <img src="{{URL::asset('/images/logo.png')}}" />
+            </a>
+            <input class="menu-btn" type="checkbox" id="menu-btn" />
+            <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+            <ul class="menu">
+                <li><a href="#work">Página inicial</a></li>
+                <li><a href="#about">Produtos</a></li>
+                <li><a href="#careers">Quem Somos</a></li>
+                <li><a href="#contact">Equipe</a></li>
+                <li><a href="#contact">Contato</a></li>
+            </ul>
+        </header>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+        <main>
+            <div class="banner">
+                <img class="banner__img" src="{{URL::asset('/images/banner.jpg')}}" alt="">
+                <div class="banner__text">
+                    <div class="banner__title">Bem-vindo(a) a Artefatos de cimentos Portão</div>
+                    <div class="banner__subtitle">Soluções em peças de concreto</div>
                 </div>
             </div>
-        </div>
+            <div class="map">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13866.287006390055!2d-51.280084!3d-29.674199!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x9b6c8edd0250cee3!2sArtefatos%20de%20Cimento%20Port%C3%A3o!5e0!3m2!1spt-BR!2sbr!4v1588943389007!5m2!1spt-BR!2sbr" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+            </div>
+        </main>
+
+        <footer class="footer">
+            Copyright © 2020 Artefatos de cimento Portão – todos os direitos reservados.
+        </footer>
+
+        <script src="{{ mix('/js/app.js') . '?v=' . time() }}"></script>
     </body>
 </html>
