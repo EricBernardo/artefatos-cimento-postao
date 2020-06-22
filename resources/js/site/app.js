e86dc6f1b3ba2c0762e82b4051af0ac367fc74a0
Eric Bernardo <eric.bernardo.sousa@gmail.com>
2020-06-22 08:57:42 -0300

    *

--- resources/js/site/app.js
+++ resources/js/site/app.js
@@ -1,37 +1,30 @@
-require('./../bootstrap');
-require('./map');
-require('./lead')
-require('./owl.carousel')
+window.$ = window.jQuery = require("jquery");
 
-const simpleParallax = require('simple-parallax-js');
+require("./map");
+require("./lead");
+require("./owl.carousel");
 
-const image = document.getElementsByClassName('banner__img');
-
-new simpleParallax(image, {
-    scale: 1.3
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        loop: false,
+        margin: 10,
+        nav: false,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 2,
+            },
+            600: {
+                items: 4,
+            },
+            1000: {
+                items: 6,
+            },
+            1400: {
+                items: 8,
+            },
+        },
+    });
 });
-
-$(document).ready(function(){
-	$('.owl-carousel').owlCarousel({
-		loop: false,
-	    margin:10,
-	    nav: false,
-	    autoplay:true,
-	    autoplayTimeout:3000,
-	    autoplayHoverPause:true,
-	    responsive:{
-	        0:{
-	            items: 2
-	        },
-	        600:{
-	            items: 4
-	        },
-	        1000:{
-	            items: 6
-	        },
-	        1400:{
-	            items: 8
-	        }
-	    }
-	});
-})
