eb0a778c930dc9824d310fda507ea649c0856a6c
Eric Bernardo <eric.bernardo.sousa@gmail.com>
2020-06-24 17:51:20 -0300

    *

--- resources/js/site/app.js
+++ resources/js/site/app.js
@@ -4,29 +4,27 @@ require("./map");
 require("./lead");
 require("./owl.carousel");
 
-window.onload = function () {
-    setTimeout(() => {
-        $(".owl-carousel").owlCarousel({
-            loop: false,
-            margin: 10,
-            nav: false,
-            autoplay: true,
-            autoplayTimeout: 3000,
-            autoplayHoverPause: true,
-            responsive: {
-                0: {
-                    items: 2,
-                },
-                600: {
-                    items: 4,
-                },
-                1000: {
-                    items: 6,
-                },
-                1400: {
-                    items: 8,
-                },
+$(window).load(function () {
+    $(".owl-carousel").owlCarousel({
+        loop: false,
+        margin: 10,
+        nav: false,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 2,
             },
-        });
-    }, 5000);
-};
+            600: {
+                items: 4,
+            },
+            1000: {
+                items: 6,
+            },
+            1400: {
+                items: 8,
+            },
+        },
+    });
+});
