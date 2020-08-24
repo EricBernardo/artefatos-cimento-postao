02a68ea35c052b12ba8fee93a9c3fd97b23f88db
Eric Bernardo <eric.bernardo.sousa@gmail.com>
2020-08-24 07:59:49 -0300

    *

--- resources/views/site/layouts/master.blade.php
+++ resources/views/site/layouts/master.blade.php
@@ -107,7 +107,7 @@
                 setTimeout(function(){
                     var s = document.createElement("script");
                     s.type = "text/javascript";
-                    s.src = "//code.jivosite.com/widget/wMTFNhDAyj";
+                    s.src = "//code.jivosite.com/widget/yUsBlummgp";
                     document.body.appendChild(s);
                 }, 1000)
             }
