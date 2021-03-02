75c82ebf805ac6951e77d835c5e41576824a5bc5
EricBernardo <eric.bernardo.sousa@gmail.com>
2021-03-02 10:50:33 -0300

    Update master.blade.php

--- resources/views/site/layouts/master.blade.php
+++ resources/views/site/layouts/master.blade.php
@@ -237,7 +237,7 @@
 				    	document.getElementById("modal-title").innerHTML = "Olá, como posso ajudar?";
 				    	document.getElementById("my-modal").style.display = 'block';
 				    }
-				}, 30000);
+				}, 20000);
 			}
             
         </script>
