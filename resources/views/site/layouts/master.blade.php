dd44ef872a18f6afc7607f540e45dc4df4e9e043
EricBernardo <eric.bernardo.sousa@gmail.com>
2021-03-10 08:36:44 -0300

    Update master.blade.php

--- resources/views/site/layouts/master.blade.php
+++ resources/views/site/layouts/master.blade.php
@@ -107,7 +107,7 @@
                 setTimeout(function(){
                     var s = document.createElement("script");
                     s.type = "text/javascript";
-                    s.src = "//code.jivosite.com/widget/Y4KF5sQMcc";
+                    s.src = "//code.jivosite.com/widget/SmPo7oDUSJ";
                     document.body.appendChild(s);
                 }, 1000)
             }
